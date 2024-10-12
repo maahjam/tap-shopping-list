d768e1212f46b49cc5ac26a9d216e1584c8ebf73
Mahsa Jamshidian <mahsa.jamshidian@digikala.com>
2024-10-12 09:33:51 +0330

    feat: create products list

---
+++ src/type/global.d.ts
@@ -0,0 +1,18 @@
+declare module '*.scss' {
+    const content: Record<string, string>;
+    export default content;
+}
+declare module '*.svg' {
+    const content: any;
+    export default content;
+}
+
+declare module '*.png' {
+    const content: string;
+    export default content;
+}
+
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
